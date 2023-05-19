3f0bca9bc79b6d6dab2bc07ff84ba005f6aedfad
Steve Davis <steve@visda.ca>
2023-05-18 22:56:52 -0400

    Output to file; remove word class

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import threading
+import json
 import sys
 from ffmpeg_audio_extractor import extract_audio
 from read_audio_chunk import read_audio_chunk
-import Word as custom_Word
 from queue import Queue
+
 
 def lambda_handler(event, context):
     queue = Queue()
@@ -14,39 +15,39 @@ def lambda_handler(event, context):
 
     ffmpeg_process = extract_audio(event["input_file"], sample_rate)
 
-    audio_thread = threading.Thread(target=read_audio_chunk, args=(ffmpeg_process, chunk_size, sample_rate, queue))
+    audio_thread = threading.Thread(target=read_audio_chunk, args=(
+        ffmpeg_process, chunk_size, sample_rate, queue))
     audio_thread.start()
 
     ffmpeg_process.wait()
 
     audio_thread.join()
 
-    list_of_words = []
     results = queue.get()
+
+    file_full_text_string = ' '.join(obj['text'] for obj in results)
+
+    output_dict = {'fullTextString': file_full_text_string, 'results': []}
     for sentence in results:
         if len(sentence) == 1:
-            # sometimes there are bugs in recognition 
-            # and it returns an empty dictionary
-            # {'text': ''}
             continue
         for obj in sentence['result']:
-            w = custom_Word.Word(obj)  # create custom Word object
-            list_of_words.append(w)  # and add it to list
+            output_dict['results'].append(obj)
 
-
-    for word in list_of_words:
-        print(word.to_string())
+    with open('output.json', 'w') as file:
+        json.dump(output_dict, file)
 
     return "success"
+
 
 def main():
     event = {
         "input_file": sys.argv[1]
     }
-    context = None 
+    context = None
     response = lambda_handler(event, context)
     print(response)
 
+
 if __name__ == "__main__":
     main()
-
